eaab824fb48c1a880fc78afcf2c95061bb5b1625
swaroopa0204 <swaroopa.neravati@gmail.com>
2024-01-23 11:27:57 -0600

    Added Test Files to the files

---
+++ src/main/java/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private String name = "Swaroopa";
+
+    public String getName() {
+        return name;
+    }
+    public static void main(String[] args){
+        Student s1 = new Student();
+        System.out.println("Name of a Student "+s1.getName());
+    }
+}
